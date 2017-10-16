19b421bbdf58ec6713da84869963036abaa7ea06
yakotta <pinkhamjenna@gmail.com>
2017-10-17 01:16:07 +0200

    created a contact form

--- templates/page_header.php
+++ templates/page_header.php
@@ -3,9 +3,9 @@
         <div class="container-fluid">
             <a class="navbar-brand" href="/"><img class="logo" src="images/luxelogo-small.png" /></a>
             <ul class="nav navbar-nav">
-                <li><a href="/">Home</a></li>
-                <li><a href="/services">Our Services</a></li>
-                <li><a href="/about">About Luxe</a></li>
+                <li><a href="<?=rewrite_url('/')?>">Home</a></li>
+                <li><a href="<?=rewrite_url('/services')?>">Our Services</a></li>
+                <li><a href="<?=rewrite_url('/about')?>">About Luxe</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Contact Us: <?=getSiteConfig("phone")?></a></li>
