04d3667870d46268a62b36596d5e3fd45f1e4597
Chris Thomas <chris.thomas@antimatter-studios.com>
2017-10-19 11:02:13 +0200

    remove the old testimonials look
    Signed-off-by: Chris Thomas <chris.thomas@antimatter-studios.com>

--- templates/block_testimonials.php
+++ templates/block_testimonials.php
@@ -5,27 +5,12 @@ if(empty($testimonial["link"])){
     $name = "<a href='{$testimonial["link"]}'>{$testimonial["name"]}</a>";
 }
 ?>
-
-<?php if(!empty($_GET["old"])): ?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?=$testimonial["name"]?></h3>
-    </div>
-    <div class="panel-body">
-        <img class="img-circle" src="<?=get_resource($testimonial["image"])?>" />
-        <p>
-            "<?=$testimonial["quote"]?>" – <?=$name?>, <?=$testimonial["title"]?>
-        </p>
-    </div>
-</div>
-<?php else: ?>
 <div class="media col-sm-4">
     <div class="media-left">
-        <img class="media-object" src="<?=get_resource($testimonial["image"])?>" alt="<?=$testimonial["name"]?>">
+        <img class="media-object" src="<?=rewrite_url($testimonial["image"])?>" alt="<?=$testimonial["name"]?>">
     </div>
     <div class="media-body">
         <h4 class="media-heading"><?=$testimonial["name"]?></h4>
         "<?=$testimonial["quote"]?>" – <?=$name?>, <?=$testimonial["title"]?>
     </div>
 </div>
-<?php endif ?>
