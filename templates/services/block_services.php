41b80d2344081ff4ace315fc295a243650e9c633
Chris Thomas <chris.thomas@antimatter-studios.com>
2017-11-14 21:07:35 +0100

    make the link for the services on the image and the text

--- templates/services/block_services.php
+++ templates/services/block_services.php
@@ -1,11 +1,15 @@
+<?php $service["link"] = rewrite_url("/services/{$service["link"]}"); ?>
+
 <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-        <img class="background"
+        <a href="<?=$service["link"]?>">
+            <img class="background"
              src="<?=rewrite_url($service["image"])?>"
              alt="<?=$service["name"]?>" />
+        </a>
 
         <div class="caption">
-            <h3><a href="<?=rewrite_url("/services/{$service["link"]}")?>"><?=$service["name"]?></a></h3>
+            <h3><a href="<?=$service["link"]?>"><?=$service["name"]?></a></h3>
         </div>
     </div>
 </div>
