74a164ad2ad34d5ad3e929b78d16683c8cd98d13
Chris Thomas <chris.thomas@antimatter-studios.com>
2017-11-14 21:09:41 +0100

    move the slider into its own template

--- templates/content_home.php
+++ templates/content_home.php
@@ -1,23 +1,14 @@
 <main>
-    <section class="clearfix slider jumbotron">
-        <div class="container-fluid">
-            <img class="background" src="<?=rewrite_url("/resources/slider/luxe-white-glove.jpg")?>" />
-            <div class="content">
-                <h1>Sliders here</h1>
-                <p>...</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-            </div>
-        </div>
-    </section>
+    <?=render_template(__DIR__."/block_slider.php")?>
 
     <section class="services container-fluid">
         <h2>Our Services</h2>
         <div class="row">
-            <?php foreach($serviceList as $service): ?>
-                <?=render_template("templates/services/block_services.php", [
+            <?php foreach($serviceList as $service){
+                print(render_template("templates/services/block_services.php", [
                     "service" => $service
-                ])?>
-            <?php endforeach?>
+                ]));
+            } ?>
         </div>
     </section>
 
@@ -33,7 +24,7 @@
     <section class="press container-fluid">
         <h2>Press</h2>
         <div class="row">
-            <?php foreach($pressList as $item){
+            <?php foreach($pressList as $press){
                 print(render_template("templates/block_press.php", [
                     "press" => $press
                 ]));
