27dd587b4f03ac31bff3a640a1a23e601507e7bf
Jenna <pinkhamjenna@gmail.com>
2018-10-12 12:20:03 -0700

    added whitespace for readability

--- templates/content_service_item.php
+++ templates/content_service_item.php
@@ -2,7 +2,9 @@
     <div class="container-fluid">
         <h1><?=$service["name"]?></h1>
         <div class="align-justify"><?=$service["full_description"]?></div>
+        
         <?php /*<img class="img-responsive" src="<?=rewrite_url($service["image"])?>" /> */ ?>
+
         <p class="go-back">
             <a href="<?=Route::rewrite_url('/services')?>">
                 <span class="glyphicon glyphicon-menu-left small"></span>Back to services
